9164268e97109c7ddc09cda180fb1dc4a48491e7
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2025-05-04 00:45:45 +0000

    [LeetCode Sync] Runtime - 273 ms (99.49%), Memory - 67.8 MB (35.25%)

---
+++ my-folder/2024-calculate-special-bonus/solution.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = np.where(
+        (employees['employee_id'] % 2 == 1) & 
+        (employees['name'].str[0] != 'M'),
+        employees['salary'],
+        0
+    )
+
+    return employees[['employee_id', 'bonus']].sort_values(by='employee_id')
