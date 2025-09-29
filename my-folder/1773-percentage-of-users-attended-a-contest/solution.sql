ec729d742db786a639459ff7d7180ca0866e5a53
H2u-Hwng <79944868+H2u-Hwng@users.noreply.github.com>
2025-09-29 06:07:05 +0000

    [LeetCode Sync] Runtime - 868 ms (88.71%), Memory - 0.0B (100.00%)

---
+++ my-folder/1773-percentage-of-users-attended-a-contest/solution.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT 
+    r.contest_id, 
+    ROUND(
+        COUNT(DISTINCT r.user_id) / (
+            SELECT COUNT(*)
+            FROM Users u
+        ) * 100
+    , 2) AS percentage
+FROM Register r
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id ASC;
